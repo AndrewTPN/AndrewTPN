b05a8786af5b08081d58d62dfa6c230c3840711f
Andrew <166637628+AndrewTPN@users.noreply.github.com>
2026-01-20 13:30:05 -0700

    update datastructure.md with full content

--- til/2026/test.py
+++ til/2026/test.py
@@ -48,14 +48,18 @@ class Solution(object):
         :rtype: bool
         """
         stack = [] 
-        mapping = {")": "(", "}": "{", "]": "["}  # These keys and values are paired brackets
+        mapping = {
+                    ")": "(", 
+                    "}": "{", 
+                    "]": "["
+                    }  # These keys and values are paired brackets
 
         # now we create a loop to iterate through each character in the string s
         for char in s:
             # if the character is one of the closing brackets
             if char in mapping:
                 # we pop the topmost element from the stack if it is non-empty, otherwise assign a dummy value of '#'
-                top_element = stack.pop() if stack else '#'
+                top_element = stack.pop() if stack else '#'  
 
                 # the mapping for the closing bracket in the string must match the top element of the stack
                 if mapping[char] != top_element:
@@ -65,3 +69,29 @@ class Solution(object):
                 stack.append(char)
         # if the stack is empty, then we have a valid expression
         return not stack
+    
+
+
+    'dictionary in python is  a collection of key-value pairs. Each key is unique and is used to store and retrieve values efficiently. Dictionaries are mutable, meaning they can be changed after their creation'
+    # Example of dictionary
+    capitals = {
+        "USA": "Washington, D.C.",
+        "France": "Paris",
+        "Japan": "Tokyo",
+        "Vietnam": "Hanoi"
+    }
+
+    # Accessing values using keys
+    print(capitals["France"]) 
+
+#List
+' [] this is a list in python, which is an ordered collection of items that can be of different types. Lists are mutable, meaning you can change their content after creation by adding, removing, or modifying elements.'
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])  # Output: banana
+'() Stack, which is a data structure that follows the Last In First Out (LIFO) principle. You can add (push) and remove (pop) items from the top of the stack.'
+'{} Set, which is an unordered collection of unique items. Sets are useful for membership testing and eliminating duplicate entries.'
+# those parentheses are used in different context in python
+my_set = {1, 2, 3, 4}
+my_list = [1, 2, 3, 4]
+my_stack = []
+my_tuple = (1, 2, 3, 4) # Tuple, which is an ordered collection of items that is immutable (cannot be changed after creation).
