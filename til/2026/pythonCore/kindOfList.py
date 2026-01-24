fa92db6fbfdcd0a9e3c493ba9be7aab074961620
Andrew <166637628+AndrewTPN@users.noreply.github.com>
2026-01-23 21:37:39 -0700

    Update kindOfList.py

--- til/2026/pythonCore/kindOfList.py
+++ til/2026/pythonCore/kindOfList.py
@@ -6,4 +6,18 @@ circular linked list
 '''
 
 # Definition for singly-linked list. 
-class ListNode(object):
+'''this is a list will content nodes, usually each node has 2 part, value and pointer to the next mode
+very easy examples of singly linked list:
+
+inside of linked list item (called node) has 2 part:
+[value | pointer]
+
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+'''
+
