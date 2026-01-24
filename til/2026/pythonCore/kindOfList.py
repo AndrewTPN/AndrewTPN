b8abcc464de52c1e2259ea9128168f03696c18fe
Andrew <166637628+AndrewTPN@users.noreply.github.com>
2026-01-23 23:06:36 -0700

    detail about linked list

--- til/2026/pythonCore/kindOfList.py
+++ til/2026/pythonCore/kindOfList.py
@@ -18,6 +18,21 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+        
+        '''
+
+# why do we use linked list compare to normal list?
+'''1. Linked list is more efficient to add and remove items in the middle of the list.
+    normal list: O(n)
+    linked list: O(1)
 
 '''
-
+# 2. Linked list is more efficient to access the items in the middle of the list.
+# 3. Linked list is more efficient to traverse the list.
+# 4. Linked list is more efficient to insert and delete items in the middle of the list.
+# 5. Linked list is more efficient to search for an item in the list.
+# 6. Linked list is more efficient to sort the list.
+# 7. Linked list is more efficient to merge two lists.
+# 8. Linked list is more efficient to split a list into two lists.
+# 9. Linked list is more efficient to reverse a list.
+# 10. Linked list is more efficient to find the middle of a list.   
